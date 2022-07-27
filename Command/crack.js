39b8526725003518cd41e69bf520090d6d971941
Argon Aliu <62722839+GO1Lock@users.noreply.github.com>
2022-07-27 16:29:24 +0200

    Update crack.js

--- Command/crack.js
+++ Command/crack.js
@@ -2,16 +2,6 @@ module.exports = {
     name: 'crack',
     description: "this is a ping command!",
     execute(message, args){
-        message.channel.send('1. tapochek.net\r\n'
-        + '2. onhax.io\r\n'
-        + '3. mazterize.com\r\n'
-        + '4. rutor.info\r\n'
-        + '5. fitgirl-repacks.site\r\n'
-        + '6. gamesforyou.co\r\n'
-        + '7. freetp.org\r\n'
-        + '8. gamestatus.info   >>>>>Cracked games status<<<<<<<\r\n'
-        + '9. gam3rb\r\n'
-        + '10. libgen.lc\r\n'
-        + '11. bowfile.com/1iHw');
+        message.channel.send();
         }
     }
